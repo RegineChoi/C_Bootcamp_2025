0a74eb00eb5c8081f18eaf0d9cac9b596f942cc9
ChoiPC <reginachoichoi@gmail>
2025-03-27 07:55:45 +0800

    LinkList4

--- 5_LinkList/LinkList_practice2.c
+++ 5_LinkList/LinkList_practice2.c
@@ -16,31 +16,36 @@ Node *initList()
     return head;
 }
 
-// // 链表插入数据-在头部插入数据
-// int insertList(Node *L, ElemType e)
-// {
-//     Node *p = (Node *)malloc(sizeof(Node));
-//     p->data = e;
-//     p->next = L->next;
-//     L->next = p;
-//     return 1;
-// }
+// 初始化节点（带节点数据域参数）
+Node *initListWithElem(ElemType e)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->data = e;
+    node->next = NULL;
+    return node;
+}
 
-// // 链表插入数据-在尾部插入数据
-// int insertList(Node *L, ElemType e)
-// {
-//     Node *p = L->next;
-//     // 续
-//     while (p != NULL)
-//     {
-//         p = p->next;
-//     }
-//     Node *temp = (Node *)malloc(sizeof(Node));
-//     temp->data = e;
-//     p->next = temp;
-//     temp->next = NULL;
-//     return 1;
-// }
+// 头插法
+int insertHead(Node *L, ElemType e)
+{
+    Node *p = (Node *)malloc(sizeof(Node));
+    p->data = e;
+    p->next = L->next;
+    L->next = p;
+    return 1;
+}
+
+// 遍历
+void displayList(Node *L)
+{
+    Node *p = L->next;
+    while (p != NULL)
+    {
+        printf("%c ", p->data);
+        p = p->next;
+    }
+    printf("\n");
+}
 
 // 尾插法
 Node *insertTail(Node *tail, ElemType e)
@@ -51,57 +56,22 @@ Node *insertTail(Node *tail, ElemType e)
     p->next = NULL;
     return p;
 }
-
-// 获取尾节点
+// 获取尾部节点
 Node *getTail(Node *L)
 {
-    Node *p = L;
-    while (p->next != NULL)
+    Node *p = L->next;
+    while (p != NULL)
     {
         p = p->next;
     }
     return p;
 }
 
-// 链表的遍历
-void displayList(Node *L)
-{
-    // 这个链表有头节点，头节点数据为0，不读取头节点的数据，只读取头节点的地址。
-    Node *p = L->next;
-    while (p != NULL)
-    {
-        printf("%c ", p->data);
-        p = p->next;
-    }
-    printf("\n");
-}
-
-// 在指定位置插入数据
-int insertElem(Node *L, int pos, ElemType e)
+// 指定位置插入
+int inserNode(Node *L, int pos, ElemType e)
 {
     Node *p = L;
     int i = 0;
-    // 遍历链表找到插入位置的前驱节点。
-    while (i < pos - 1)
-    {
-        p = p->next;
-        i++;
-    }
-    // 要插入的新节点设置为q
-    Node *q = (Node *)malloc(sizeof(Node));
-    q->data = e;
-    q->next = p->next;
-    p->next = q;
-    return 1;
-}
-
-// 删除节点
-int deleteElem(Node *L, int pos)
-{
-
-    Node *p = L->next;
-    int i = 1;
-    // 遍历链表，找到要删除节点的前驱。
     while (i < pos - 1)
     {
         p = p->next;
@@ -109,55 +79,38 @@ int deleteElem(Node *L, int pos)
         if (p == NULL)
             return 0;
     }
-    // q指向删除节点
+    Node *q = (Node *)malloc(sizeof(Node));
+    q->data = e;
+    q->next = p->next;
+    p->next = q;
+}
+
+// 删除节点
+int deleteNode(Node *L, int pos)
+{
+    Node *p = L;
+    int i = 0;
+    while (i < pos - 1)
+    {
+        p = p->next;
+        i++;
+    }
     Node *q = p->next;
     p->next = q->next;
-    // 释放要删除节点的内存空间
     free(q);
     return 1;
 }
 
-// 获取链表长度
-int listLength(Node *L)
+// 尾插法（节点）
+Node *insertTailWithNode(Node *tail, Node *node)
 {
-    int length = 0;
-    Node *p = L->next;
-    while (p != NULL)
-    {
-        p = p->next;
-        length++;
-    }
-    return length;
-}
-
-// 释放链表
-void freeList(Node *L)
-{
-    Node *p = L->next;
-    Node *q;
-    while (p != NULL)
-    {
-        q = p->next;
-        free(p);
-        p = q;
-    }
-    L->next = NULL;
+    tail->next = node;
+    node->next = NULL;
+    return node;
 }
 
 int main()
 {
-    Node *list = initList();
-    Node *tail = getTail(list);
-    tail = insertTail(tail, 'c');
-    tail = insertTail(tail, 'h');
-    tail = insertTail(tail, 'a');
-    displayList(list);
-    insertElem(list, 3, 'r');
-    displayList(list);
-    deleteElem(list, 3);
-    displayList(list);
-    printf("%d\n", listLength(list));
-    freeList(list);
-    printf("%d", listLength(list));
+
     return 0;
 }
