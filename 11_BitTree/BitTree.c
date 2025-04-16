81dd2330c88673da4586956f92eef65da2f122a1
ChoiPC <reginachoichoi@gmail>
2025-04-16 08:20:29 +0800

    BitTree1

--- 11_BitTree/BitTree.c
+++ 11_BitTree/BitTree.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef char ElemType;
+typedef struct TreeNode
+{
+    ElemType data;
+    struct TreeNode *lchild;
+    struct TreeNode *rchild;
+} TreeNode;
+
+// 重命名TreeNode指针为biTree
+typedef TreeNode *biTree;
+
+// 二叉树的前序遍历
+void preOrder(biTree T)
+{
+    if (T == NULL)
+        return;
+    printf("%c", T->data);
+    preOrder(T->lchild);
+    preOrder(T->rchild);
+}
+
 int main()
 {
     return 0;
