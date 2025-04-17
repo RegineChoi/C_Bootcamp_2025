e9b35ede6e8ef68a72cd292980cbe43644112e02
ChoiMatebookE <reginachoichoi@gmail>
2025-04-17 17:58:45 +0800

    BiTree4

--- 11_BitTree/BitTree_SecondRankPointer_Modify.c
+++ 11_BitTree/BitTree_SecondRankPointer_Modify.c
@@ -9,14 +9,11 @@ typedef struct TreeNode
     struct TreeNode *rchild;
 } TreeNode;
 
-// 重命名TreeNode指针为biTree
-typedef TreeNode *biTree;
-
 char str[] = "ABDH#K###E##CFI###G#J##";
 int index = 0;
 
-// 传入一个biTree指针T，相当于TreeNode指针的指针
-void createTree(biTree *T)
+// TreeNode指针的指针
+void createTree(TreeNode **T)
 {
     ElemType ch;
     ch = str[index++];
@@ -26,7 +23,7 @@ void createTree(biTree *T)
     }
     else
     {
-        *T = (biTree)malloc(sizeof(TreeNode));
+        *T = (TreeNode *)malloc(sizeof(TreeNode));
         (*T)->data = ch;
         createTree(&(*T)->lchild);
         createTree(&(*T)->rchild);
@@ -34,7 +31,7 @@ void createTree(biTree *T)
 }
 
 // 二叉树的前序遍历
-void preOrder(biTree T)
+void preOrder(TreeNode *T)
 {
     if (T == NULL)
         return;
@@ -44,7 +41,7 @@ void preOrder(biTree T)
 }
 
 // 二叉树的中序遍历
-void inOrder(biTree T)
+void inOrder(TreeNode *T)
 {
     if (T == NULL)
         return;
@@ -54,7 +51,7 @@ void inOrder(biTree T)
 }
 
 // 二叉树的后序遍历
-void postOrder(biTree T)
+void postOrder(TreeNode *T)
 {
     if (T == NULL)
         return;
@@ -65,7 +62,7 @@ void postOrder(biTree T)
 
 int main()
 {
-    biTree T;       // TreeNode指针
+    TreeNode *T;    // TreeNode指针
     createTree(&T); // 传入TreeNode指针的地址
     preOrder(T);
     printf("\n");
