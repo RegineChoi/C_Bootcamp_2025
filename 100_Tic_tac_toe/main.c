750f00c552b2d1e870dae8db725198aad6b24038
ChoiMatebookE <reginachoichoi@gmail>
2025-04-21 23:50:33 +0800

    20250421作业

--- 100_Tic_tac_toe/main.c
+++ 100_Tic_tac_toe/main.c
@@ -6,6 +6,9 @@
 
 #define SIZE 3
 
+#define HUMAN 'X'
+#define COMPUTER 'O'
+
 // 棋盘初始化
 void init_board(char board[SIZE][SIZE])
 {
@@ -80,15 +83,156 @@ void get_move(int *row, int *col)
     }
 }
 
+// 电脑下棋
+void ai_move(char board[SIZE][SIZE], int *row, int *col)
+{
+    do
+    {
+        *row = rand() % SIZE;
+        *col = rand() % SIZE;
+    } while (board[*row][*col] != ' ');
+}
+
+// 判断胜负
+int check_win(char board[SIZE][SIZE])
+{
+    // 检查行
+    for (int i = 0; i < SIZE; i++)
+    {
+        if (board[i][0] == ' ')
+            continue;
+        bool win = true; // 假设这一行已经连成一行
+        for (int j = 1; j < SIZE; j++)
+        {
+            if (board[i][j] != board[i][0])
+            {
+                win = false;
+                break;
+            }
+        }
+        if (win)
+            return 1;
+    }
+
+    // 检查列
+    for (int j = 0; j < SIZE; j++)
+    {
+
+        if (board[0][j] == ' ')
+            continue;
+        bool win = true;
+        for (int i = 1; i < SIZE; i++)
+        {
+            if (board[i][j] != board[0][j])
+            {
+                win = false;
+                break;
+            }
+        }
+        if (win)
+            return 1;
+    }
+
+    // 检查主对角线
+    if (board[0][0] != ' ')
+    {
+        bool win = true;
+        for (int i = 1; i < SIZE; i++)
+        {
+            if (board[i][i] != board[0][0])
+            {
+                win = false;
+                break;
+            }
+        }
+        if (win)
+            return 1;
+    }
+
+    // 检查副对角线
+    if (board[0][SIZE - 1] != ' ')
+    {
+        bool win = true;
+        for (int i = 1; i < SIZE; i++)
+        {
+            if (board[i][SIZE - 1 - i] != board[0][SIZE - 1])
+            {
+                win = false;
+                break;
+            }
+        }
+        if (win)
+            return 1;
+    }
+
+    // 检查平局
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (board[i][j] == ' ')
+                return 0; // 有空位继续游戏
+        }
+    }
+
+    return 2; // 平局
+}
+
 int main()
 {
     char board[SIZE][SIZE];
-    init_board(board);
-    print_board(board);
+    char current_player = HUMAN;
+    int game_status = 0;
 
-    int row = 0;
-    int col = 0;
-    get_move(&row, &col);
+    init_board(board);
+    srand(time(NULL));
+    while (1)
+    {
+        print_board(board);
+        if (current_player == HUMAN)
+        {
+            // 玩家下棋
+            int row = 0;
+            int col = 0;
+            get_move(&row, &col);
+
+            if (board[row][col] == ' ')
+            {
+                board[row][col] = HUMAN;
+            }
+            else
+            {
+                printf("该位置已被占用,重新输入\n");
+                continue;
+            }
+        }
+        else
+        {
+            // 电脑下棋
+            int row = 0;
+            int col = 0;
+            ai_move(board, &row, &col);
+            board[row][col] = COMPUTER;
+            printf("电脑下在：%d行 %d列\n", row + 1, col + 1);
+        }
+
+        game_status = check_win(board);
+
+        if (game_status == 1)
+        {
+            print_board(board);
+            printf("%s获胜！\n", (current_player == HUMAN) ? "玩家" : "电脑");
+            break;
+        }
+        else if (game_status == 2)
+        {
+            print_board(board);
+            printf("平局！\n");
+            break;
+        }
+        current_player = (current_player == HUMAN) ? COMPUTER : HUMAN;
+    }
+
     system("pause");
     return 0;
 }
